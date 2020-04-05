e043a77ca708c73ba7290cf6e6c3930d7067a943
root <root@10.0.2.15>
2020-04-05 00:08:09 -0400

    Use with Wireshark

---
+++ wireshark_example.sh
@@ -0,0 +1,25 @@
+file=${1:-wireshark_example.txt}
+interface=${2:-enp0s8}
+
+gcc send_by_string.c -o sbs
+
+[[ $1 == "-h" ]] && {
+  echo "Usage: $0 [file] [interface]"
+  exit 0
+}
+
+[ -f $file ] || {
+  echo $file: No such file
+  exit 1
+}
+
+str=`cat $file |awk '{$1="";print $0 }' | sed ":a;N;s/\\n//g;ta" |sed s/" "//g`
+
+dst=${str:0:12}
+src=${str:12:12}
+data=${str:24}
+echo $src
+echo $dst
+echo $data
+
+./sbs $interface $dst $data $src
