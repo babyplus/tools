7f3a86f397b877eedfc7b4325c4a01b21ba9b5ee
babyplus <maxcrazy@yeah.net>
2021-10-17 06:17:59 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,72 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import xlrd
+from xlutils.copy import copy
+import sys
+
+
+def _handle(cell):
+    # ctype： 0 empty,1 string, 2 number, 3 date, 4 boolean, 5 error
+    # if 0 == cell_0.ctype:
+    #     value_0 = ""
+    # elif 1 == cell_0.ctype:
+    #     value_0 = cell_0.value
+    # elif 2 == cell_0.ctype and 0 == cell_0.value % 1:
+    #     print("整数")
+    #     value_0 = str(int(cell_0.value))
+    # elif 2 == cell_0.ctype and 0 != cell_0.value % 1:
+    #     print("浮点数")
+    #     value_0 = str(cell_0.value)
+    return "" if 0 == cell.ctype else (cell.value if 1 == cell.ctype else (
+            str(int(cell.value)) if (2 == cell.ctype and 0 == cell.value % 1) else str(cell.value)))
+
+
+def handle(cell):
+    return _handle(cell).strip()
+
+
+def read(change_xls):
+    workbook = xlrd.open_workbook(change_xls)
+    sheet = workbook.sheet_by_index(0)
+    rows = sheet.nrows
+    for row in range(0, rows):
+        word = [handle(sheet.row(row)[0]), handle(sheet.row(row)[1])]
+        words.append(word)
+
+
+def change(excel_file_name, sheet_name, target_file_name):
+    workbook = xlrd.open_workbook(excel_file_name)
+    sheet = workbook.sheet_by_name(sheet_name)
+    rows = sheet.nrows
+    cols = sheet.ncols
+    copybook = copy(workbook)
+    copybook_sheet = copybook.get_sheet(sheet_name)
+
+    for row in range(0, rows):
+        for col in range(0, cols):
+            content = handle(sheet.cell(row, col))
+            content_size = len(content)
+            print(content_size)
+            print(content)
+            for word in words:
+                word_size = len(word[0])
+                print(word_size)
+                print(word)
+                if -1 != content.find(word[0]) and word_size == content_size:
+                    copybook_sheet.write(row, col, word[1])
+
+    copybook.save(target_file_name)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    words = []
+    # python main.py "excel_file_name" "sheet_name" "change_file_name" "target_file_name"
+    read(sys.argv[1:][2])
+    print(words)
+    change(sys.argv[1:][0], sys.argv[1:][1], sys.argv[1:][3])
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
