8fa91473889864a8726a24ac108af24361be43b9
babyplus <maxcrazy@yeah.net>
2021-10-18 03:37:35 +0000

    fix bug: upsupported ctype

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import xlrd
 from xlutils.copy import copy
+from xlutils.filter import process, XLRDReader, XLWTWriter
 import sys
 
 
@@ -20,8 +16,13 @@ def _handle(cell):
     # elif 2 == cell.ctype and 0 != cell.value % 1:
     #     print("浮点数")
     #     value = str(cell.value)
-    return "" if 0 == cell.ctype else (cell.value if 1 == cell.ctype else (
-            str(int(cell.value)) if (2 == cell.ctype and 0 == cell.value % 1) else str(cell.value)))
+    return "" if 0 == cell.ctype else (
+        cell.value if 1 == cell.ctype else (
+            str(int(cell.value)) if (2 == cell.ctype and 0 == cell.value % 1) else (
+                str(cell.value) if (2 == cell.ctype and 0 != cell.value % 1) else (
+                    # return "" if the type is unsupported.
+                    ""
+                ))))
 
 
 def handle(cell):
@@ -37,27 +38,40 @@ def read(change_xls):
         words.append(word)
 
 
+def style_list_copy(wb):
+    w = XLWTWriter()
+    process(
+        XLRDReader(wb, 'unknown.xls'),
+        w
+        )
+    return w.style_list
+
+
 def change(excel_file_name, sheet_name, target_file_name):
-    workbook = xlrd.open_workbook(excel_file_name)
+    workbook = xlrd.open_workbook(excel_file_name, formatting_info=True)
     sheet = workbook.sheet_by_name(sheet_name)
     rows = sheet.nrows
     cols = sheet.ncols
+
     copybook = copy(workbook)
+    style_list = style_list_copy(workbook)
     copybook_sheet = copybook.get_sheet(sheet_name)
 
     for row in range(0, rows):
         for col in range(0, cols):
+            style = style_list[sheet.cell_xf_index(row, col)]
             content = handle(sheet.cell(row, col))
+            if "" == content:
+                continue
             content_size = len(content)
             for word in words:
                 word_size = len(word[0])
                 if -1 != content.find(word[0]) and word_size == content_size:
-                    copybook_sheet.write(row, col, word[1])
+                    copybook_sheet.write(row, col, word[1], style)
 
     copybook.save(target_file_name)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     '''
     example:
@@ -67,5 +81,3 @@ if __name__ == '__main__':
     read(sys.argv[1:][2])
     print(words)
     change(sys.argv[1:][0], sys.argv[1:][1], sys.argv[1:][3])
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
