8c90c7cadca20ae354748ace62e44659e6e5ddf4
babyplus <maxcrazy@yeah.net>
2022-02-21 11:38:03 +0000

    Export 60 days of data.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,17 @@
-path="`pwd`"
-project_path=${path%/*}
-date=`date +%Y%m%d`
 data_path=${1:-/tmp}
-data_file=${2:-$data_path/test_$date.txt}
-output=${3:-$data_path/images/$date/}
-url=${4:-http://172.17.0.1/pic/}
-mkdir -p $output
-docker run -v $data_path:/tmp --net host -it export_client node export_images.js --output $output --data $data_file --url $url 
+url=${2:-http://172.17.0.1/pic/}
+data_file_prefix=${3:-test_}
+data_file_suffix=${4:-.txt}
+
+for i in {1..60}
+do
+    date=`date +%Y%m%d -d "-${i}day"`
+    data_file=$data_path/${data_file_prefix}${date}${data_file_suffix}
+    [ ! -f $data_file ] || {
+        output=$data_path/images/$date/
+        [ -d $output ] || {
+            mkdir -p $output
+            docker run -v $data_path:$data_path --net host -it export_client node export_images.js --output $output --data $data_file --url $url
+        }
+    }
+done
